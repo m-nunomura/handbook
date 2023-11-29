894d1f797758fe9e3d76e432a8b4c1c1fd9fffdb
djangouser <m-nunomura@stroog.com>
2023-11-29 15:17:21 +0900

    20231129_5-3end

--- 20231129/blogproject/blogapp/admin.py
+++ 20231129/blogproject/blogapp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+# modelsをインポート
 from . import models
 
 # Register your models here.
 
+# Django管理サイトにBlogPostを登録する
 admin.site.register(models.BlogPost)
--- 20231129/blogproject/blogapp/views.py
+++ 20231129/blogproject/blogapp/views.py
@@ -3,19 +3,30 @@ from django.shortcuts import render
 # django.viewsからgenericをインポート
 from django.views import generic
 
+# modelsをインポート
+from . import models
+
 # Create your views here.
 
-class IndexView(generic.TemplateView):
+class IndexView(generic.ListView):
     #トップページのビュー
 
     '''
-    テンプレートのレンダリングに特化したTemplateViewを継承
+    投稿記事を一覧表示するのでListViewを継承する
 
     Attributes:
-        template_name: レンダリングするテンプレート   
+        template_name: レンダリングするテンプレート
+        context_object_name: object_listキーの別名を設定
+        queryset: データベースのクエリ   
     '''
     # index.htmlをレンダリングする
     template_name = "blogapp/index.html"
+
+    # object_listキーの別名を設定
+    context_object_name = "orderby_records"
+
+    #モデルBlogPostのオブジェクトにorder_by()を適用してBlogPostのレコードを投稿日時の降順で並び替える
+    queryset = models.BlogPost.objects.order_by("-posted_at")
 
 
 
@@ -36,9 +47,20 @@ class IndexView(generic.TemplateView):
 def index_view(request):
     # トップページのビュー
     '''
+    テンプレートをレンダリングして戻り値として返す
+
     Parameters:
-        request(HTTPRequest): クライアントからのリクエスト情報
+        request(HTTPRequest): クライアントからのリクエスト情報を格納したHTTPRequestオブジェクト
+
     Returns(HTTPResponse):
         render()でテンプレートをレンダリングした結果
     '''
-    return render(request,"blogapp/index.html")
+
+    # モデルBlogPostのオブジェクトにorderby()を適用してBlogPostのレコードを投稿日時の降順で並べ替える
+    records = models.BlogPost.objects.order_by("-posted_at")
+
+    # render()：
+    # 第1引数：HttpRequestオブジェクト
+    # 第2引数：レンダリングするテンプレート
+    # 第3引数：テンプレートに引き渡すdict型のデータ{任意のキー:クエリの結果（レコードのリスト）}
+    return render(request,"blogapp/index.html",{"orderby_records":records})
