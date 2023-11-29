1ba1feae9216bf284fb5a2a8a3e3417ba792479d
djangouser <m-nunomura@stroog.com>
2023-11-29 15:49:40 +0900

    20231129_5-4end

--- 20231129/blogproject/blogapp/urls.py
+++ 20231129/blogproject/blogapp/urls.py
@@ -6,9 +6,32 @@ app_name = "blogapp"
 
 # URLパターンを登録するためのリスト
 urlpatterns = [
+    
     # http(s)://ホスト名/以下のパスが""（無し）の場合
     # viewsモジュールのIndexViewを実行
     # URLパターン名は"index"
     path("",views.IndexView.as_view(),name="index"),
-    path("func/",views.index_view,name="index")
+    
+    # リクエストされたURLが「blog-detail/レコードのid」の場合
+    # viewsモジュールのBlogDetailを実行
+    # URLパターン名を"blog_detail"にする
+    path("blog-detail/<int:pk>/",views.BlogDetail.as_view(),name="blog_detail"),
+
+
+
+
+
+
+
+
+
+
+
+    ## function ##
+
+    # http(s)://ホスト名/以下のパスが"func"の場合
+    # viewsモジュールのindex_view()を実行
+    # URLパターン名は"index"
+    path("func/",views.index_view,name="index"),
+    path("func/blog-detail/<int:pk>/",views.blog_detail,name="blog_detail"),
 ]
--- 20231129/blogproject/blogapp/views.py
+++ 20231129/blogproject/blogapp/views.py
@@ -19,6 +19,7 @@ class IndexView(generic.ListView):
         context_object_name: object_listキーの別名を設定
         queryset: データベースのクエリ   
     '''
+    
     # index.htmlをレンダリングする
     template_name = "blogapp/index.html"
 
@@ -27,6 +28,25 @@ class IndexView(generic.ListView):
 
     #モデルBlogPostのオブジェクトにorder_by()を適用してBlogPostのレコードを投稿日時の降順で並び替える
     queryset = models.BlogPost.objects.order_by("-posted_at")
+
+
+
+class BlogDetail(generic.DeleteView):
+    # 詳細ページのビュー
+
+    '''
+    投稿記事の詳細を表示するのでDetailViewを継承する
+
+    Attributes:
+        template_name: レンダリングするテンプレート
+        Model: モデルのクラス
+    '''
+
+    # post.htmlをレンダリングする
+    template_name = "blogapp/post.html"
+
+    # クラス変数modelにモデルBlogPostを設定
+    model = models.BlogPost
 
 
 
@@ -64,3 +84,29 @@ def index_view(request):
     # 第2引数：レンダリングするテンプレート
     # 第3引数：テンプレートに引き渡すdict型のデータ{任意のキー:クエリの結果（レコードのリスト）}
     return render(request,"blogapp/index.html",{"orderby_records":records})
+
+
+
+def blog_detail(request,pk):
+    # 詳細ページのビュー
+    '''
+    テンプレートをレンダリングして戻り値として返す
+
+    Parameters:
+        request(HTTPRequest): クライアントからのリクエスト情報を格納したHTTPRequestオブジェクト
+
+        pk(int): 投稿記事のレコードのid
+
+    Returns(HttpResponse):
+        テンプレートpost.htmlをレンダリングした結果
+    '''
+
+    # モデルのマネージャをBlogPost.objectsで参照し、get()を実行
+    # 引数に指定したidのレコードを取得してrecordに格納
+    record = models.BlogPost.objects.get(id=pk)
+
+    # render():
+    # 第1引数：HttpRequestオブジェクト
+    # 第2引数：レンダリングするテンプレート
+    # 第3引数：テンプレートに引き渡すdict型のデータ{任意のキー:get()で取得したレコード}
+    return render(request,"blogapp/post.html",{"object":record})
