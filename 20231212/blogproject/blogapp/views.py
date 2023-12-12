2843d8a5e2479e9a2463995377dee1c317dbee05
m-nunomura <m-nunomura@stroog.com>
2023-12-12 16:29:31 +0900

    20231212_4-3end

--- 20231212/blogproject/blogapp/views.py
+++ 20231212/blogproject/blogapp/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 from django.views import generic
 # Create your views here.
 
+
+'''
 class IndexView(generic.TemplateView):
-    pass
+    template_name = "blog/index.html"
+'''
+
+
+def index_view(request):
+    return render(request,"blog/index.html")
