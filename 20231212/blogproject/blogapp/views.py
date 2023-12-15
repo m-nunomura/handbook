b0a774ffa356a5b1421c06083f7001524fdc3cbe
m-nunomura <m-nunomura@stroog.com>
2023-12-15 09:51:01 +0900

    20231215_5-3end

--- 20231212/blogproject/blogapp/views.py
+++ 20231212/blogproject/blogapp/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from django.views import generic
+from . import models
 # Create your views here.
 
 
-
-class IndexView(generic.TemplateView):
+#'''
+class IndexView(generic.ListView):
     template_name = "blog/index.html"
+    context_object_name = "orderby_records"
+    queryset = models.BlogPost.objects.order_by("-posted_at")
+#'''
+
 
 
 '''
 def index_view(request):
-    return render(request,"blog/index.html")
-
+    records = models.BlogPost.objects.order_by("-posted_at")
+    context = {
+        "orderby_records":records
+    }
+    return render(request,"blog/index.html",context)
 '''
