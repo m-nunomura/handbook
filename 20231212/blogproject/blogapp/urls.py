6e626f59ff216a73862459e92400556d8d12394d
m-nunomura <m-nunomura@stroog.com>
2023-12-15 14:42:37 +0900

    20231215_6-1end

--- 20231212/blogproject/blogapp/urls.py
+++ 20231212/blogproject/blogapp/urls.py
@@ -3,23 +3,25 @@ from . import views
 
 app_name = "blogapp"
 
-'''
+#'''
 urlpatterns = [
     path("",views.IndexView.as_view(),name="index"),
     path("blog-detail/<int:pk>/",views.BlogDetail.as_view(),name="blog_detail"),
     path("science-list/",views.ScienceView.as_view(),name="science_list"),
     path("dailylife-list/",views.DailylifeView.as_view(),name="dailylife_list"),
     path("music-list/",views.MusicView.as_view(),name="music_list"),
+    path("contact/",views.ContactView.as_view(),name="contact"),
 ]
-'''
-
-
 #'''
+
+
+'''
 urlpatterns = [
     path("",views.index_view,name="index"),
     path("blog-detail/<int:pk>/",views.blog_detail,name="blog_detail"),
     path("science-list/",views.science_view,name="science_list"),
     path("dailylife-list/",views.dailylife_view,name="dailylife_list"),
     path("music-list/",views.music_view,name="music_list"),
+    path("contact/",views.contact_view,name="contact"),
 ]
-#'''
+'''
--- 20231212/blogproject/blogapp/views.py
+++ 20231212/blogproject/blogapp/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
 from django.views import generic
 from django.core.paginator import Paginator
-from . import models,consts
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.core.mail import EmailMessage
+from . import models,consts,forms
 # Create your views here.
 
 
-'''
+#'''
 class IndexView(generic.ListView):
     template_name = "blog/index.html"
     context_object_name = "orderby_records"
@@ -33,11 +37,34 @@ class MusicView(generic.ListView):
     context_object_name = "music_records"
     queryset = models.BlogPost.objects.filter(category="music").order_by("-posted_at")
     paginate_by = consts.PAGINATE_CATE    
-'''
 
+class ContactView(generic.FormView):
+    template_name = "blog/contact.html"
+    form_class = forms.ContactForm
+    success_url = reverse_lazy("blogapp:contact")
 
+    def form_valid(self, form):
+        name = form.cleaned_data["name"]
+        email = form.cleaned_data["email"]
+        title = form.cleaned_data["title"]
+        message = form.cleaned_data["message"]
+        subject = "お問い合わせ：{}".format(title)
+        message = "送信者：{0}\nメールアドレス：{1}\nタイトル：{2}\nメッセージ：{3}".format(name,email,title,message)
+        from_email = "admin@example.com"
+        to_list = ["admin@example.com"]
+        message = EmailMessage(subject=subject,
+                               body=message,
+                               from_email=from_email,
+                               to=to_list,)
+        message.send()
+        messages.success(self.request,"お問い合わせは正常に送信されました。")
+        return super().form_valid(form)
 
 #'''
+
+
+
+'''
 def index_view(request):
     records = models.BlogPost.objects.order_by("-posted_at")
     paginator = Paginator(records,consts.PAGINATE)
@@ -88,5 +115,31 @@ def music_view(request):
         "page_obj":pages,
     }
     return render(request,"blog/music_list.html",context)
-#'''
+
+def contact_view(request):
+    if request.method == "GET":
+        form = forms.ContactForm()
+        context = {
+            "form":form,
+        }
+        return render(request,"blog/contact.html",context)
+    else:
+        form = forms.ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            email = form.cleaned_data["email"]
+            title = form.cleaned_data["title"]
+            message = form.cleaned_data["message"]
+            subject = "お問い合わせ：{}".format(title)
+            message = "送信者：{0}\nメールアドレス：{1}\nタイトル：{2}\nメッセージ：{3}".format(name,email,title,message)
+            from_email = "admin@example.com"
+            to_list = ["admin@example.com"]
+            message = EmailMessage(subject=subject,
+                                body=message,
+                                from_email=from_email,
+                                to=to_list,)
+            message.send()
+            messages.success(request,"お問い合わせは正常に送信されました。")
+            return redirect("blogapp:contact")
+'''
 
