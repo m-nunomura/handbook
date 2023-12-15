5c5d5ff7c44d9368b7bfde0f72a0fe440542d792
m-nunomura <m-nunomura@stroog.com>
2023-12-15 15:25:01 +0900

    20231215_7-1end

---
+++ 20231212/photoproject/photo/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path,include
+from . import views
+
+app_name = "photo"
+
+#'''
+urlpatterns = [
+    path("",views.IndexView.as_view(),name="index"),
+]
+#'''
+
+
+'''
+urlpatterns = [
+    path("",views.index_view,name="index"),
+]
+'''
---
+++ 20231212/photoproject/photo/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views import generic
+
+# Create your views here.
+
+#'''
+class IndexView(generic.TemplateView):
+    template_name = "photo/index.html"
+#'''
+
+'''
+def index_view(request):
+    return render(request,"photo/index.html")
+'''
